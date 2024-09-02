d860ae3e8e70a47edd179d894e860aa4ebcb7b61
Ahmed Zahir <xahiru@gmail.com>
2024-09-02 20:50:50 +0800

    warm up exercise 1

---
+++ C/warm_up_ex_1.c
@@ -0,0 +1,8 @@
+/*
+Warm up exercise 1
+1: Write a program that reads a positive integer n and prints the sum of the first n positive integers.
+2: Given an array and an element, write a function to insert the element at a specified position.
+3: Write a function to delete an element from an array at a specified position.Write a function to delete an element from an array at a specified position.
+4: Write a function to search for the first occurrence of an element in an array and return its position.
+5: Write a function that finds and returns the maximum element in an array.
+*/
