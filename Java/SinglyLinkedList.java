6f47c049aabe040cacb9ea272625a07a5495f4fb
Ahmed Zahir <xahiru@gmail.com>
2024-09-02 21:25:01 +0800

    singly linked list java file added

---
+++ Java/SinglyLinkedList.java
@@ -0,0 +1,72 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    Node head;
+
+    // Insert an element at the end of the list
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    // Delete the first occurrence of an element
+    public void delete(int data) {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        if (head.data == data) {
+            head = head.next;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null && temp.next.data != data) {
+            temp = temp.next;
+        }
+        if (temp.next != null) {
+            temp.next = temp.next.next;
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+
+    // Traverse and print the elements of the list
+    public void traverse() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        System.out.println("List after insertion:");
+        list.traverse();
+
+        list.delete(3);
+        System.out.println("List after deletion:");
+        list.traverse();
+    }
+}
