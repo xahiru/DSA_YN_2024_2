42e866e323754de9da0667f62b224a0eed679e84
Ahmed Zahir <xahiru@gmail.com>
2024-09-02 21:37:36 +0800

    delete repeated

--- C/warm_up_ex_1.c
+++ C/warm_up_ex_1.c
@@ -2,7 +2,7 @@
 Warm up exercise 1
 1: Write a function to initialize an array of size 10 with the first 10 positive integers.
 2: Given an array and an element, write a function to insert the element at a specified position.
-3: Write a function to delete an element from an array at a specified position.Write a function to delete an element from an array at a specified position.
+3: Write a function to delete an element from an array at a specified position.
 4: Write a function to search for the first occurrence of an element in an array and return its position.
 5: Write a function that finds and returns the maximum element in an array.
 */
