f441ca4986dd4d5fe1afb98782233356466c3634
Ahmed Zahir <xahiru@gmail.com>
2024-10-10 22:00:35 +0800

    answer 1 added

---
+++ C/prcatical_sessions/q1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+// Main function
+int main()  
+{  
+    int arr[10];  // Declare an array of size 10 to store integer values
+    int i;  
+    // Print a message to prompt the user for input
+    printf("\n\nRead and Print elements of an array:\n");
+    printf("-----------------------------------------\n");	
+    // Prompt the user to input 10 elements into the array
+    printf("Input 10 elements in the array :\n");  
+    for(i=0; i<10; i++)  
+    {  
+	    printf("element - %d : ",i);  // Prompt the user to input the i-th element
+        scanf("%d", &arr[i]);  // Read the input and store it in the array
+    }  
+    // Display the elements in the array
+    printf("\nElements in array are: ");  
+    for(i=0; i<10; i++)  
+    {  
+        printf("%d  ", arr[i]);  // Print each element in the array
+    } 
+    printf("\n");
+	return 0;	
+}
